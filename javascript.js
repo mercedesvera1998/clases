818433a2dc52eed7f6077a5ac559919805c77875
mercedesvera1998 <108768147+mercedesvera1998@users.noreply.github.com>
2022-07-16 18:33:51 -0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-let nombre = "Programación" + " " + "Fácil.";
-let saludo = "Les doy la bienvenida a";
-let suerte = "Les deseo mucha suerte";
-let fraseCompleta = saludo + " " + nombre + " "+ suerte;
-
-console.log(fraseCompleta);
-
-
