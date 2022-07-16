4145e04ae83be7534211fea8e33a6db58b07bbf1
mercedesvera1998 <108768147+mercedesvera1998@users.noreply.github.com>
2022-07-16 18:17:51 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let nombre = "Programación" + " " + "Fácil.";
+let saludo = "Les doy la bienvenida a";
+let suerte = "Les deseo mucha suerte";
+let fraseCompleta = saludo + " " + nombre + " "+ suerte;
+
+console.log(fraseCompleta);
+
+
